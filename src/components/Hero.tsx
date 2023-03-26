218cf9f32b2f5f2acb2f5c98362958c68a0c0237
Yami <cursedlawliet@gmail.com>
2023-03-26 07:33:33 +0400

    fix the flex bug

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,29 +6,31 @@ type Props = {};
 
 const Hero = (props: Props) => {
   return (
-    <div className="h-screen flex flex-col justify-center items-center space-y-8 overflow-hidden">
+    <div className="h-screen flex flex-col justify-center items-center space-y-8 overflow-hidden text-center">
       <BackgroundCircles />
       <img
         className="w-32 h-32 rounded-full object-cover object-top"
         src="/me.JPG"
         alt="me"
       />
-      <Typewriter
-        words={[
-          "Talks About",
-          "Javascript",
-          "React",
-          "Next.js",
-          "Typescript",
-          "Node.js",
-        ]}
-        loop={0}
-        cursor
-        cursorStyle="/"
-        typeSpeed={70}
-        deleteSpeed={50}
-        delaySpeed={1000}
-      />
+      <div>
+        <Typewriter
+          words={[
+            "Talks About",
+            "Javascript",
+            "React",
+            "Next.js",
+            "Typescript",
+            "Node.js",
+          ]}
+          loop={0}
+          cursor
+          cursorStyle="/"
+          typeSpeed={70}
+          deleteSpeed={50}
+          delaySpeed={1000}
+        />
+      </div>
     </div>
   );
 };
